192be8a5b774920458ecbaec0c59496ac743f749
Eric <mountriv99@yahoo.ca>
2019-04-11 20:40:18 +0800

    added list_transcode_template_group

--- lib/aliyun/vod/media_processing/transcode_template.rb
+++ lib/aliyun/vod/media_processing/transcode_template.rb
@@ -5,6 +5,32 @@ module Aliyun
 
 				class << self
 
+					# https://help.aliyun.com/document_detail/102669.html
+					# returns:  {
+					#   "RequestId": "25818875-5F78-4A13-BEF6-D7393642CA58",
+					#   "TranscodeTemplateGroupList": [
+					#     {
+					#       "Name": "test",
+					#       "IsDefault": "NotDefault",
+					#       "CreationTime": "2018-12-05T10:20:09Z",
+					#       "ModifyTime": "2018-12-05T10:20:09Z",
+					#       "TranscodeTemplateGroupId": "17a9889fc6685261a81d791c886700932"
+					#     },
+					#     {
+					#       "Name": "transcode",
+					#       "IsDefault": "NotDefault",
+					#       "CreationTime": "2018-12-05T10:20:09Z",
+					#       "ModifyTime": "2018-12-05T10:20:09Z",
+					#       "TranscodeTemplateGroupId": "86e5a2a3c28a9299b8cbab7fe0882b1c9"
+					#     }
+					#   ]
+					# }
+					def list_transcode_template_group
+						params = params.merge({:Action => "ListTranscodeTemplateGroup"})
+
+						Util.get_response(params)
+					end
+
 					# https://www.alibabacloud.com/help/zh/doc-detail/102665.htm
 					# params: {
 					#   :Name => "my_template", # optional
